7b7fdbf63d4bb5137fdf78a3723c5485da8d1f8b
hitari <hitari@naver.com>
2021-05-09 22:45:14 +0900

    Tab 구성 변경

--- src/features/rhymeList/RhymeList.tsx
+++ src/features/rhymeList/RhymeList.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
 import RhymeItem from './RhymeItem';
 
 interface List {
@@ -12,48 +12,28 @@ interface Props {
 }
 
 const RhymeList = ({ list }: Props) => {
-  const koCount = 0;
-  const enCount = 0;
-
   return (
-    <article id="content">
-      <h3 className="tab_result_left selected">
-        <a href="#content" className="tit_tab">
-          한글
-          <span className="num_result" title="전체단어수">
-            ({koCount})
-          </span>
-        </a>
-      </h3>
-      <h3 className="tab_result_right">
-        <a href="#content" className="tit_tab">
-          영어
-          <span className="num_result" title="전체단어수">
-            ({enCount})
-          </span>
-        </a>
-      </h3>
-      <section className="box_result selected">
-        <p className="box_paging">
-          <strong className="num_selected">1</strong>
-          <a className="num_paging" data-page-id="2">
-            2
-          </a>
-          <a className="btn_next num_paging" data-page-id="6">
-            다음<span className="txt_arrow">▶</span>
-          </a>
-          <span className="num_total" title="전체 페이지수">
-            22
-          </span>
-        </p>
-        <ul>
-          {list.map((item) => (
-            <RhymeItem key={item.no} subject={item.subject} mean={item.mean} />
-          ))}
-        </ul>
-      </section>
-    </article>
+    <ul>
+      {list.map((item) => (
+        <RhymeItem key={item.no} subject={item.subject} mean={item.mean} />
+      ))}
+    </ul>
   );
 };
 
 export default RhymeList;
+
+{
+  /* <p className="box_paging">
+<strong className="num_selected">1</strong>
+<a className="num_paging" data-page-id="2">
+  2
+</a>
+<a className="btn_next num_paging" data-page-id="6">
+  다음<span className="txt_arrow">▶</span>
+</a>
+<span className="num_total" title="전체 페이지수">
+  22
+</span>
+</p> */
+}
--- src/features/rhymeList/RhymeListPage.tsx
+++ src/features/rhymeList/RhymeListPage.tsx
@@ -1,21 +1,48 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { RootState } from '@/app/rootReducer';
 import { useSelector, useDispatch } from 'react-redux';
+import classnames from 'classnames';
 import { fetchRhymeList } from './RhymeListSlice';
 import RhymeList from './RhymeList';
 
 const RhymeListPage = () => {
   const dispatch = useDispatch();
   const { list } = useSelector((state: RootState) => state.rhymeList);
+  const [tab, setTab] = useState('ko');
+  const koCount = 0;
+  const enCount = 0;
+
+  const onclickTab = useCallback((e) => setTab(e.currentTarget.dataset.id), [tab]);
 
   useEffect(() => {
     dispatch(fetchRhymeList({}));
   }, []);
 
   return (
-    <div>
-      <RhymeList list={list} />
-    </div>
+    <article id="content">
+      <div className={classnames('tab_result_left', { selected: tab === 'ko' })}>
+        <a href="#" className="tit_tab" data-id="ko" onClick={onclickTab}>
+          한글
+          <span className="num_result" title="전체단어수">
+            ({koCount})
+          </span>
+        </a>
+      </div>
+      <div className={classnames('tab_result_right', { selected: tab === 'en' })}>
+        <a href="#" className="tit_tab" data-id="en" onClick={onclickTab}>
+          영어
+          <span className="num_result" title="전체단어수">
+            ({enCount})
+          </span>
+        </a>
+      </div>
+      <section className={classnames('box_result', { selected: tab === 'ko' })}>
+        <RhymeList list={list} />
+      </section>
+      <section className={classnames('box_result', { selected: tab === 'en' })}>
+        <RhymeList list={list} />
+      </section>
+    </article>
   );
 };
 
